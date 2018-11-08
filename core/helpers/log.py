b351a620fd77709e5993b7bbda3996532537e649
ainiml <ericjaw@gmail.com>
2018-11-08 05:09:34 -0500

    Updated logger format + adds module name it originates from

--- core/helpers/log.py
+++ core/helpers/log.py
@@ -4,10 +4,12 @@ from datetime import datetime
 
 FORMAT = '%(asctime)s %(levelname)s %(threadName)s %(module)s  %(message)s'
 FORMAT = '[%(asctime)s][%(levelname)s ][%(threadName)s] [%(module)s] %(message)s'
+FORMAT = '%(asctime)s [%(levelname)s ] %(message)s'
+FORMAT = '[%(levelname)s ] %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 
-def log(message):
+def log(message, module=None):
     """ Default logging format
 
     """
@@ -15,5 +17,8 @@ def log(message):
         # logging.info('{} '.format(datetime.now().isoformat()))
         logging.info('')
     else:
-        # logging.info('{}  {}'.format(datetime.now().isoformat(), message))
-        logging.info('{}'.format(message))
+        if module:
+            # logging.info('{}  {}'.format(datetime.now().isoformat(), message))
+            logging.info('[{}] {}'.format(module, message))
+        else:
+            logging.info('{}'.format(message))

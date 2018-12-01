6c8cf8cd3c43b7aeea3e2bc91922d4e7da11bd28
ainiml <ericjaw@gmail.com>
2018-12-01 16:18:46 -0500

    Added hevlog

--- core/helpers/log/logger.py
+++ core/helpers/log/logger.py
@@ -1,25 +1,53 @@
 import logging
-from datetime import datetime
+
+from logging import INFO, DEBUG, ERROR, WARNING
 
 
-FORMAT = '%(asctime)s %(levelname)s %(threadName)s %(module)s  %(message)s'
-FORMAT = '[%(asctime)s][%(levelname)s ][%(threadName)s] [%(module)s] %(message)s'
-FORMAT = '%(asctime)s [%(levelname)s ] %(message)s'
+# Set logger format
+# FORMAT = '%(asctime)s %(levelname)s %(threadName)s %(module)s  %(message)s'
+# FORMAT = '[%(asctime)s][%(levelname)s ][%(threadName)s] [%(module)s] %(message)s'
+# FORMAT = '%(asctime)s [%(levelname)s ] %(message)s'
 FORMAT = '[%(levelname)s ] %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+# TODO: need to find out which logging config is used when there are multiple ones
+logging.basicConfig(format=FORMAT, level=DEBUG)
 
 
-def log(message, module=None):
-    """ Default logging format
+def log(message=None, module=None, log_type=None):
+    """ Default logging
 
     """
     if not message:
-        # logging.info('{} '.format(datetime.now().isoformat()))
         logging.info('')
     else:
         if module:
-            # logging.info('{}  {}'.format(datetime.now().isoformat(), message))
-            logging.info('[{}] {}'.format(module, message))
+            if log_type is INFO:
+                logging.info('[{}] {}'.format(module, message))
+            if log_type is DEBUG:
+                logging.debug('[{}] {}'.format(module, message))
+            if log_type is ERROR:
+                logging.error('[{}] {}'.format(module, message))
+            if log_type is WARNING:
+                logging.warning('[{}] {}'.format(module, message))
         else:
             logging.info('{}'.format(message))
 
+
+def hevlog(message=None, module=None, log_type=None):
+    """ Default HEV logging
+
+    """
+    if not message:
+        logging.info('')
+    else:
+        if module:
+            if log_type is INFO:
+                logging.info('[{}] {}'.format(module, message))
+            if log_type is DEBUG:
+                logging.debug('[{}] {}'.format(module, message))
+            if log_type is ERROR:
+                logging.error('[{}] {}'.format(module, message))
+            if log_type is WARNING:
+                logging.warning('[{}] {}'.format(module, message))
+        else:
+            logging.info('{}'.format(message))

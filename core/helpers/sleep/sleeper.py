5c9fa7a48ff7ef76cc8c6b6f1d0567bca11478d2
ainiml <ericjaw@gmail.com>
2019-01-12 19:47:22 -0500

    + added hour sleep

--- core/helpers/sleep/sleeper.py
+++ core/helpers/sleep/sleeper.py
@@ -6,7 +6,17 @@ from core.helpers.log import hevlog
 hevlog = hevlog('sleeper', level='debug')
 
 
+def hour(caller):
+    """At some time within an hour, this will run
+    """
+    sleep = random.choice(range(1, 1 * 60 * 60))
+    hevlog.logging.debug('[{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
 def day(caller):
+    """At some time within 24 hours, this will run
+    """
     sleep = random.choice(range(1, 24 * 60 * 60))
     hevlog.logging.debug('[{}] sleeping for {} seconds'.format(caller, sleep))
     return time.sleep(sleep)

28f60d127932f3c4a01c9d5c8b10b8dfc4754d7a
ainiml <ericjaw@gmail.com>
2019-07-14 20:29:27 -0400

    fixed tests

---
+++ tests/test_instagram.py
@@ -0,0 +1,12 @@
+import json
+
+from modules import instagram
+
+
+def test_run():
+    CONF = json.load(open('../hev.conf'))
+    instagram.test_run(CONF['instagram'])
+
+
+if __name__ == "__main__":
+    test_run()

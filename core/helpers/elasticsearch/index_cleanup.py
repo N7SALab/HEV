2cb1828af5d6827a5877cb8d3571cb42bdd8e4c2
ainiml <ericjaw@gmail.com>
2019-07-18 20:57:51 -0400

    lower elasticsearch retention from 30 to 14 days

---
+++ core/helpers/elasticsearch/index_cleanup.py
@@ -0,0 +1,166 @@
+import datetime
+import elasticsearch
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+
+from core.helpers.log import hevlog
+from core.helpers.sleep import sleeper
+
+hevlog = hevlog('elasticsearch', level='info')
+
+
+class ElasticsearchConnect:
+
+    def __init__(self, host=['elasticsearch'], request_timeout=10,
+                 http_auth=None, use_ssl=True, verify_certs=True,
+                 connection_class=RequestsHttpConnection):
+        """
+
+        :param host:
+        :param request_timeout:
+        :param http_auth:
+        :param use_ssl:
+        :param verify_certs:
+        :param connection_class:
+        """
+
+        for _host in host:
+            try:
+                self.wrapper = Elasticsearch(
+                    hosts=[_host],
+                    request_timeout=request_timeout,
+                    http_auth=http_auth,
+                    use_ssl=use_ssl,
+                    verify_certs=verify_certs,
+                    connection_class=connection_class
+                )
+                break
+            except:
+                self.wrapper = None
+            finally:
+                if self.wrapper is None:
+                    hevlog.logging.error('No elasticsearch hosts available')
+                    raise Exception('No elasticsearch hosts available')
+
+        self.host = host
+        self.cache = []
+        self.indices = []
+
+    def search_indices(self, index_pattern):
+        try:
+            retrieved_indices = self.wrapper.indices.get(index_pattern)
+            num_indices = len(retrieved_indices)
+
+            msg = 'Search found {} indices'.format(num_indices)
+            hevlog.logging.info('[search indices] {}'.format(msg))
+            return retrieved_indices
+        except elasticsearch.exceptions.NotFoundError:
+            msg = '''You provided the index pattern '{}', but searches returned fruitless'''
+            msg = msg.format(index_pattern)
+            hevlog.logging.error('[search indices] {}'.format(msg))
+
+    def delete_indices(self, index_pattern):
+
+        retrieved_indices = [x for x in self.search_indices(index_pattern).keys()]
+        num_indices = len(retrieved_indices)
+
+        msg = 'Search found {} indices'.format(num_indices)
+        hevlog.logging.info('[delete indices] {}'.format(msg))
+
+        if not num_indices:
+            msg = '''No indices found. exiting'''
+            print(msg)
+            return False
+
+        for index in retrieved_indices:
+            print(index)
+
+        # TODO: Find a way to undo index deletions
+        #       One way could be to rename the indices and store a link to the new
+        #       indices in a node of deleted indices
+        if num_indices < 2:
+            msg = '''\nYOU'RE ABOUT TO DELETE {} INDEX! ARE YOU SURE YOU WANT TO CONTINUE? '''
+        elif num_indices > 1:
+            msg = '''\nYOU'RE ABOUT TO DELETE {} INDICES! ARE YOU SURE YOU WANT TO CONTINUE? '''
+        msg += '''THIS CANNOT BE UNDONE! DECIDE WISELY [y/N]'''
+        msg = msg.format(num_indices)
+        print(msg)
+
+        answer = str(input()).lower()
+
+        if not answer:
+            answer = 'N'
+
+        if answer == 'y':
+            for index in retrieved_indices:
+                msg = '''Deleting {}...'''
+                msg = msg.format(index)
+                print(msg, end='')
+                # Delete the index
+                self.wrapper.indices.delete(index=index)
+                print('done')
+        else:
+            msg = '''Whew, you might have just blew it, if you had said yes'''
+            print(msg)
+
+    def get_indices(self):
+        retrieved_indices = self.wrapper.indices.get('*')
+        num_indices = len(retrieved_indices)
+
+        self.indices = retrieved_indices
+        msg = 'Retrieved {} indices'.format(num_indices)
+        hevlog.logging.debug('[get indices] {}'.format(msg))
+
+
+def run(elasticsearch_config):
+    hevlog.logging.info('Running...')
+
+    # TODO: this might create too many connections to elasticsearch
+    while True:
+
+        es = ElasticsearchConnect(elasticsearch_config['hosts'], use_ssl=False, request_timeout=40)
+
+        hevlog.logging.debug('[elasticsearch cleaner] {}'.format(es.wrapper.info()))
+        hevlog.logging.debug('[elasticsearch cleaner] {}'.format(es.get_indices()))
+
+        DAYS = 14
+
+        pattern = '*'
+        search = es.search_indices(pattern)
+        keys = sorted(list(search.keys()))
+
+        # ignore these indices
+        ignore = [
+            '.kibana',
+            '.kibana_1',
+            '.kibana_2',
+            '.kibana_3'
+        ]
+
+        for _key in ignore:
+            if _key in keys:
+                keys.remove(_key)
+
+        for alias in keys:
+            # indices = get_indice(es, alias)
+            indices = es.wrapper.indices.get(alias)
+
+            creation_date = indices[alias]['settings']['index']['creation_date']
+            creation_date = int(creation_date)
+
+            # month old
+            month = datetime.timedelta(days=DAYS)
+            today = datetime.datetime.today()
+            past = today - month
+            epoch = past - datetime.datetime.utcfromtimestamp(0)
+            delete_older = int(epoch.total_seconds()) * 1000
+
+            if creation_date < delete_older:
+                # delete index
+                # es.indices.delete(alias, ignore=[400, 404])
+                es.wrapper.indices.delete(alias)
+                hevlog.logging.info('[elasticsearch cleaner] deleted {}'.format(alias))
+
+        hevlog.logging.info('[ElasticsearchConnect] done')
+        hevlog.logging.debug('[ElasticsearchConnect] sleeping')
+        sleeper.day('elasticsearch cleanup')

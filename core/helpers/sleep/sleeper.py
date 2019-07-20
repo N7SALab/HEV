a1c2b3208cf7a4b037bfb3462d6b73d8853916db
ainiml <naisanza@gmail.com>
2019-07-19 23:58:07 -0400

    added a sleeper range function

---
+++ core/helpers/sleep/sleeper.py
@@ -0,0 +1,54 @@
+import time
+import random
+
+from core.helpers.log import hevlog
+
+hevlog = hevlog('sleeper', level='info')
+
+
+def seconds(caller, seconds):
+    """Sleep for this many seconds
+    """
+    sleep = seconds
+    hevlog.logging.debug('[{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
+def minute(caller):
+    """Sleep for a random minute
+    """
+    sleep = random.choice(range(1, 1 * 60))
+    hevlog.logging.debug('[{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
+def minutes(caller, minutes):
+    """Sleep for this many minutes
+    """
+    sleep = minutes * 60
+    hevlog.logging.debug('[mins] [{}] sleeping for {} minutes'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
+def hour(caller):
+    """At some time within an hour, this will run
+    """
+    sleep = random.choice(range(1, 1 * 60 * 60))
+    hevlog.logging.info('[hour] [{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
+def day(caller):
+    """At some time within 24 hours, this will run
+    """
+    sleep = random.choice(range(1, 24 * 60 * 60))
+    hevlog.logging.info('[day] [{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)
+
+
+def range(caller, seconds):
+    """Sleep for a random range
+    """
+    sleep = random.choice(range(1, seconds))
+    hevlog.logging.debug('[range] [{}] sleeping for {} seconds'.format(caller, sleep))
+    return time.sleep(sleep)

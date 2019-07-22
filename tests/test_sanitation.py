f68facd4cf98b93609d0f8868a37d811faee564f
ainiml <naisanza@gmail.com>
2019-07-22 15:54:27 -0400

    added input sanitation

---
+++ tests/test_sanitation.py
@@ -0,0 +1,19 @@
+from core.helpers import sanitation
+
+
+def test_filename():
+    test = 'handle-it.jpeg'
+    result = test
+    assert sanitation.string(test) == result
+
+
+def test_simple_filename():
+    test = 'handle.jpeg'
+    result = test
+    assert sanitation.string(test) == result
+
+
+def test_bad_filename():
+    test = 'handle:118//$1&1>2.jpeg'
+    result = 'handle_118___1_1_2.jpeg'
+    assert sanitation.string(test) == result

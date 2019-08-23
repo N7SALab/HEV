0a5aa218e7ee31d23a067f7184db7e9a48b61ae6
ainiml <naisanza@gmail.com>
2019-08-23 11:47:06 -0400

    added list sanitation

--- tests/test_sanitation.py
+++ tests/test_sanitation.py
@@ -17,3 +17,9 @@ def test_bad_filename():
     test = 'handle:118//$1&1>2.jpeg'
     result = 'handle_118___1_1_2.jpeg'
     assert sanitation.string(test) == result
+
+
+def test_dedup_list():
+    test = [1, 1, 1, 1, 2, 3, 4]
+    result = [1, 2, 3, 4]
+    assert sanitation.dedup_list(test) == result

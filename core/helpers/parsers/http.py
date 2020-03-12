45e20b249c8a7caf6489327e91767a08f0e55e74
ainiml <naisanza@gmail.com>
2020-03-12 01:15:38 -0400

    updated http parser

--- core/helpers/parsers/http.py
+++ core/helpers/parsers/http.py
@@ -1,14 +1,17 @@
-from core.helpers.neo4j import helpers
+from core.helpers import neo4j
+
+
+# from core.helpers import crypto
 
 
 def http_header(headers):
     # [print(x) for x in auth.request_headers(request)]
 
-    # token = helper_brain.hash_key(sorted([x for x in headers]))
+    # token = crypto.hash_key(sorted([x for x in headers]))
 
     args = dict(
         blob=sorted(headers),
         label='Headers'
     )
 
-    helpers.prepare_dict(**args)
+    neo4j.prepare_dict(**args)

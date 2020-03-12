5fe12fbca36f08451cbbaed05f1bc445308f689a
ainiml <naisanza@gmail.com>
2020-03-12 00:23:31 -0400

    use git tag for versioning

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ set -e
 
 cd $(dirname $0)
 
-pytest tests || exit 1
-
 DOCKERFILE="Dockerfile"
 
 if [ ! $(which docker) ]; then echo "*** missing docker, please install docker ***"; exit 1; fi
@@ -15,11 +13,10 @@ if [ ! -f $DOCKERFILE ]; then echo "*** missing $DOCKERFILE ***"; exit 1; fi
 
 # get dockername
 STR=$(cat $DOCKERFILE | grep LABEL | grep dockername) || { echo "failed"; exit 1; }
-DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit   1; }
+DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit 1; }
 
 # get dockertag
-STR=$(cat $DOCKERFILE | grep LABEL | grep dockertag) || { echo "failed"; exit 1; }
-DOCKERTAG=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit    1; }
+DOCKERTAG=$(git describe --tags)
 
 set -ex
 
@@ -27,12 +24,20 @@ set -ex
 docker build -t $DOCKERNAME:$DOCKERTAG .
 docker tag $DOCKERNAME:$DOCKERTAG $DOCKERNAME:latest
 
+# test image
+#docker run --rm --entrypoint "/usr/local/bin/pytest" $DOCKERNAME:$DOCKERTAG "/hev/tests"
+# big shm-size for chrome to work
+docker run --rm --shm-size 2g --entrypoint "/usr/local/bin/pytest" $DOCKERNAME:$DOCKERTAG "/hev/tests"
+
 # push image
-REGISTRY="rancher.n7sa.com:5000"
-docker tag $DOCKERNAME $REGISTRY/$DOCKERNAME:$DOCKERTAG
-docker tag $DOCKERNAME $REGISTRY/$DOCKERNAME:latest
-docker push $REGISTRY/$DOCKERNAME:$DOCKERTAG
-docker push $REGISTRY/$DOCKERNAME:latest
+docker push $DOCKERNAME:$DOCKERTAG
+
+# push image
+#REGISTRY="registry:5000"
+#docker tag $DOCKERNAME $REGISTRY/$DOCKERNAME:$DOCKERTAG
+#docker tag $DOCKERNAME $REGISTRY/$DOCKERNAME:latest
+#docker push $REGISTRY/$DOCKERNAME:$DOCKERTAG
+#docker push $REGISTRY/$DOCKERNAME:latest
 
 # list image
-docker images $DOCKERNAME
+docker images | grep $DOCKERNAME
